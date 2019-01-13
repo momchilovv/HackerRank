55e130f7afe1120aba88deef5291b162e7f80371
Cvetelin Momchilov <cecko04@hotmail.com>
2019-01-13 13:40:09 +0200

    Uploading Day 9 and Day 10.

---
+++ 30 Days of Code/Day10.BinaryNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day10.BinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string binary = Convert.ToString(n, 2);
+
+            int count = 0;
+            int longestSequence = 0;
+
+            foreach (var item in binary)
+            {
+                if (item == '1')
+                {
+                    count++;
+                    if (count > longestSequence)
+                    {
+                        longestSequence = count;
+                    }
+                }
+                else if (item == '0')
+                {
+                    if (count > longestSequence)
+                    {
+                        longestSequence = count;
+                    }
+                    count = 0;
+
+                }
+            }
+            Console.WriteLine(longestSequence);
+        }
+    }
+}
---
+++ 30 Days of Code/Day9.Recursion3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day9.Recursion3
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Factorial(n);
+
+            Console.WriteLine(Factorial(n));
+        }
+    }
+}
