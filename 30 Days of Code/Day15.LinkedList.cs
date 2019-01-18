e439504a5cea4201d5230192a3f2002e29f17b74
Cvetelin Momchilov <cecko04@hotmail.com>
2019-01-18 21:43:42 +0200

    Finally adding Day 15 - Linked List.

---
+++ 30 Days of Code/Day15.LinkedList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Day15.LinkedList
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class Program
+    {
+        public static Node insert(Node head, int data)
+        {
+            if (head == null)
+            {
+                return new Node(data);
+            }
+            else if (head.next == null)
+            {
+                head.next = new Node(data);
+            }
+            else
+            {
+                insert(head.next, data);
+            }
+            return head;
+        }
+
+        static void display(Node head)
+        {
+            Node start = head;
+
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Node head = null;
+
+            int T = int.Parse(Console.ReadLine());
+
+            while (T --> 0)
+            {
+                int data = int.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
+        }
+    }
+}
