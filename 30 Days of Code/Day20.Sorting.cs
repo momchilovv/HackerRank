585e1c5e487153282b01f1d8cc56ac85476a6752
Cvetelin Momchilov <cecko04@hotmail.com>
2019-01-18 21:36:56 +0200

    Uploading Days 19-21.

---
+++ 30 Days of Code/Day20.Sorting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Day20.Sorting
+{
+    class Program
+    {
+        static void swap(ref int firstPos,ref int secondPos)
+        {
+            int temp = firstPos;
+            firstPos = secondPos;
+            secondPos = temp;
+
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int numberOfSwaps = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    if (intArr[j] > intArr[j + 1])
+                    {
+                        swap(ref intArr[j],ref intArr[j + 1]);
+                        numberOfSwaps++;
+                    }
+                }
+                if (numberOfSwaps == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Array is sorted in {numberOfSwaps} swaps.");
+            Console.WriteLine($"First Element: {intArr.First()}");
+            Console.WriteLine($"Last Element: {intArr.Last()}");
+        }
+    }
+}
---
+++ 30 Days of Code/Day21.Generics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Day21.Generics
+{
+    class Program
+    {
+        private static void PrintArray<T>(T[] stringArray)
+        {
+            foreach (var item in stringArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] intArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            n = int.Parse(Console.ReadLine());
+            string[] stringArray = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                stringArray[i] = Console.ReadLine();
+            }
+
+            PrintArray<int>(intArray);
+            PrintArray<string>(stringArray);
+        }
+    }
+}
